0c9cbd63e0aaa31992799317cce88b473fbfa7cd
imikuclud <15378259919@163.com>
2022-04-06 16:27:29 +0800

    完善token信息更新操作

--- src/request/RefreshTokenRequest.php
+++ src/request/RefreshTokenRequest.php
@@ -16,5 +16,56 @@ class RefreshTokenRequest extends Request
      */
     private $route = '/oauth/refresh_token';
 
+    /**
+     * refreshToken 数据信息
+     */
+    private $refreshToken = '';
+
+    /**
+     * 设置接口路由
+     * @param string $route
+     */
+    public function setRoute( string $route)
+    {
+        $this->route = $route;
+    }
+
+    /**
+     * 获取接口路由
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * 设置接口路由
+     * @param string $refreshToken
+     */
+    public function setRefreshToken(string $refreshToken)
+    {
+        $this->refreshToken = $refreshToken;
+    }
+
+    /**
+     * 获取接口路由
+     */
+    public function getRefreshToken()
+    {
+        return $this->refreshToken;
+    }
+
+
+    /**
+     * 整合请求需要的参数
+     */
+    public function fieldOption()
+    {
+        $data['refreshToken'] = $this->getRefreshToken();
+        $data = array_filter($data);
+
+        $this->setFields($data);
+    }
+
 
 }
